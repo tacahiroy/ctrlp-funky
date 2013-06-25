f44f067d65de8a69c4bece47bc3dafba3d956f45
tacahiroy <tacahiroy@gmail.com>
2013-06-25 21:10:56 +0900

    Add sample file

---
+++ test/funky.py
@@ -0,0 +1,24 @@
+class Vim:
+    def editor():
+        print "Vi Improved"
+
+class CtrlP:
+    def method():
+        print "ctrlp!"
+
+
+class Funky(Vim, CtrlP):
+    def funk():
+        print "funk"
+
+    def sturday(night, fever):
+        print "dancing"
+
+    def sleeping(on,
+                 sunday):
+        print "zzZ"
+
+    def monday(
+            ):
+        print "absent"
+
